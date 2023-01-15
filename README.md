ce73e926b7ae209244fc6e0ea6b67c86064e55f5
paulaandrezza <paulaandrezza25@gmail.com>
2023-01-15 20:23:37 -0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Countdown
+<h1 align="center"> Countdown </h1>
+
+<p align="center">
+    Esse projeto tem como objetivo exibir uma contagem regressiva até o dia do meu aniversário que é em 25/03
+</p>
+
+<br>
+
+<p align="center">
+    <img alt="Countdown" src="./assets/img/projeto.jpeg" width="100%">
+</p>
+
+<br>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript
+
+Você pode ver o projeto [clicando aqui](https://paulaandrezza.github.io/Countdown/).
+
+<br>
+
+## 🔖 Layout
+
+Você pode visualizar o layout do projeto através [DESSE LINK]("https://www.figma.com/file/dO7o34Il8TV7XP6yoYFu8e/DD-%2F-Countdown-(Copy)?node-id=2001%3A2&t=zLawHsaRzJZ5DuEl-0/duplicate"). É necessário ter conta no [Figma](https://figma.com) para acessá-lo.
---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+const day = document.getElementById('day');
+const hour = document.getElementById('hour');
+const minute = document.getElementById('minute');
+const second = document.getElementById('second');
+
+var now = new Date();
+var birthday = new Date(2023, 02, 25, 00, 00, 00); // o mes considera de 0 a 11, logo o mes 3 que é o meu caso virou 2
+
+var x = setInterval(function() {
+
+    // Obtém a data/hora atual
+    var now = new Date().getTime();
+  
+    // Encontra a distância entre a data atual e a data final
+    var distance = birthday - now;
+  
+    // Calcula dias, horas, minutos e segundos
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    day.innerHTML = days.toString().padStart(2, '0');
+    hour.innerHTML = hours.toString().padStart(2, '0');
+    minute.innerHTML = minutes.toString().padStart(2, '0');
+    second.innerHTML = seconds.toString().padStart(2, '0');    
+
+}, 1000);
